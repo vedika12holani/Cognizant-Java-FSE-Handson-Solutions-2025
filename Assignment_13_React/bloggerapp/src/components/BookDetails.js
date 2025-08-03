e89a1d6758ff920bcef40f7801808010e4f11961
vedika12holani <vedikaholani@gmail.com>
2025-08-03 12:09:39 +0530

    Removed submodules and added folders as normal directories

---
+++ Assignment_13_React/bloggerapp/src/components/BookDetails.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function BookDetails({ books }) {
+  return (
+    <div className="st2">
+      <h1>Book Details</h1>
+      {books.map((book) => (
+        <div key={book.id}>
+          <h3>{book.bname}</h3>
+          <h4>{book.price}</h4>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default BookDetails;
