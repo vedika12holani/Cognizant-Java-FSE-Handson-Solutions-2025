30666e96a57d3f892584df24d6a5ba7ccbbe2a8d
vedika12holani <160023582+vedika12holani@users.noreply.github.com>
2025-08-05 21:41:34 +0530

    Delete Week_7/Week_3 directory

--- Week_7/Week_3/Spring_Core_Maven/Exercise1_BasicSpringApplication/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-
-    public String getBookData() {
-        return "Book: Effective Java, Author: Joshua Bloch, Price: Rs.450\n";
-    }
-}
